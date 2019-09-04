fc5f16466f3cff1acab703984432ce6a090f7932
Jean-Frederic Clere <jfclere@gmail.com>
2019-09-04 17:54:11 +0200

    Add RPI4 host list. Add the shell to install the docker images

---
+++ cloud/infra.install.sh
@@ -0,0 +1,5 @@
+docker pull                   docker.io/cockpit/kubernetes:latest,
+docker pull                   docker.io/openshift/origin-deployer:v3.11,
+docker pull                   docker.io/openshift/origin-docker-registry:v3.11,
+docker pull                   docker.io/openshift/origin-haproxy-router:v3.11,
+docker pull                   docker.io/openshift/origin-pod:v3.11
---
+++ cloud/master.install.sh
@@ -0,0 +1,7 @@
+docker pull        docker.io/cockpit/kubernetes:latest
+docker pull        docker.io/openshift/origin-control-plane:v3.11
+docker pull        docker.io/openshift/origin-deployer:v3.11
+docker pull        docker.io/openshift/origin-docker-registry:v3.11
+docker pull        docker.io/openshift/origin-haproxy-router:v3.11
+docker pull        docker.io/openshift/origin-pod:v3.11
+docker pull        quay.io/coreos/etcd:v3.2.22
