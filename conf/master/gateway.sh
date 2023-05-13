080a464e0d3460565a146447fe752fea6f7fad98
Jean-Frederic Clere <jfclere@gmail.com>
2023-05-13 09:52:58 +0100

    Arange it to run it every minutes via crontab

--- conf/master/gateway.sh
+++ conf/master/gateway.sh
@@ -1,24 +1,33 @@
+#!/bin/bash 
+
 #service firewalld stop
+
+a=`cat /proc/sys/net/ipv4/ip_forward`
+if [ $a == "1" ]; then
+  # already done
+  echo "Done..."
+  exit 0
+fi
 
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
 #iptables-restore gateway.ok.saintismier
 #iptables-restore gateway.ok
-ifconfig wlan1 | grep 10.0.0.201
+/usr/sbin/ifconfig wlan1 | grep 10.0.0.201
 if [ $? -eq 0 ]; then
   # wlan1 is configurated to forward wifi.
-  iptables -P FORWARD ACCEPT
-  iptables --table nat -A POSTROUTING -o wlan0 -j MASQUERADE
+  /usr/sbin/iptables -P FORWARD ACCEPT
+  /usr/sbin/iptables --table nat -A POSTROUTING -o wlan0 -j MASQUERADE
 
-  iptables -A FORWARD -i wlan0 -o wlan1 -m state --state RELATED,ESTABLISHED -j ACCEPT
-  iptables -A FORWARD -i wlan1 -o wlan0 -j ACCEPT
+  /usr/sbin/iptables -A FORWARD -i wlan0 -o wlan1 -m state --state RELATED,ESTABLISHED -j ACCEPT
+  /usr/sbin/iptables -A FORWARD -i wlan1 -o wlan0 -j ACCEPT
 
 else
-  iptables -P FORWARD ACCEPT
-  iptables --table nat -A POSTROUTING -o wlan1 -j MASQUERADE
+  /usr/sbin/iptables -P FORWARD ACCEPT
+  /usr/sbin/iptables --table nat -A POSTROUTING -o wlan1 -j MASQUERADE
 
-  iptables -A FORWARD -i wlan1 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-  iptables -A FORWARD -i wlan0 -o wlan1 -j ACCEPT
+  /usr/sbin/iptables -A FORWARD -i wlan1 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+  /usr/sbin/iptables -A FORWARD -i wlan0 -o wlan1 -j ACCEPT
 
 fi 
 #something like: (Configure wlan1 to be a WiFi Client)
