260b28299e06eb7b545e925bdea10f61f30c4e55
Jean-Frederic Clere <jfclere@gmail.com>
2023-05-22 17:20:10 +0100

    Arrange the logic to have a moving wlan0/1.

--- conf/master/gateway.sh
+++ conf/master/gateway.sh
@@ -1,35 +1,57 @@
 #!/bin/bash 
 
+ORGNET=eth0
+DESTNET=eth0
+
 #service firewalld stop
 
 a=`cat /proc/sys/net/ipv4/ip_forward`
 if [ $a == "1" ]; then
   # already done
-  echo "Done..."
   exit 0
+fi
+
+# find the network that is connected to the internet
+/usr/sbin/ifconfig wlan0 | grep 192.168
+if [ $? -eq 0 ]; then
+  ORGNET=wlan0
+fi
+/usr/sbin/ifconfig wlan1 | grep 192.168
+if [ $? -eq 0 ]; then
+  ORGNET=wlan1
+fi
+/usr/sbin/ifconfig alf0 | grep 192.168
+if [ $? -eq 0 ]; then
+  ORGNET=alf0
+fi
+
+/usr/sbin/ifconfig wlan1 | grep 10.0.0.201
+if [ $? -eq 0 ]; then
+  DESTNET=wlan1
+fi
+/usr/sbin/ifconfig wlan0 | grep 10.0.0.201
+if [ $? -eq 0 ]; then
+  DESTNET=wlan0
+fi
+/usr/sbin/ifconfig alf0 | grep 10.0.0.201
+if [ $? -eq 0 ]; then
+  DESTNET=alf0
+fi
+
+if [ $DESTNET == ${ORGNET} ]; then
+  echo "Something wrong... $DESTNET"
+  exit 1
 fi
 
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
-#iptables-restore gateway.ok.saintismier
-#iptables-restore gateway.ok
-/usr/sbin/ifconfig wlan1 | grep 10.0.0.201
-if [ $? -eq 0 ]; then
-  # wlan1 is configurated to forward wifi.
-  /usr/sbin/iptables -P FORWARD ACCEPT
-  /usr/sbin/iptables --table nat -A POSTROUTING -o wlan0 -j MASQUERADE
+echo "$DESTNET to ${ORGNET}"
+/usr/sbin/iptables -P FORWARD ACCEPT
+/usr/sbin/iptables --table nat -A POSTROUTING -o ${ORGNET} -j MASQUERADE
 
-  /usr/sbin/iptables -A FORWARD -i wlan0 -o wlan1 -m state --state RELATED,ESTABLISHED -j ACCEPT
-  /usr/sbin/iptables -A FORWARD -i wlan1 -o wlan0 -j ACCEPT
+/usr/sbin/iptables -A FORWARD -i ${ORGNET} -o ${DESTNET} -m state --state RELATED,ESTABLISHED -j ACCEPT
+/usr/sbin/iptables -A FORWARD -i ${DESTNET} -o ${ORGNET} -j ACCEPT
 
-else
-  /usr/sbin/iptables -P FORWARD ACCEPT
-  /usr/sbin/iptables --table nat -A POSTROUTING -o wlan1 -j MASQUERADE
-
-  /usr/sbin/iptables -A FORWARD -i wlan1 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-  /usr/sbin/iptables -A FORWARD -i wlan0 -o wlan1 -j ACCEPT
-
-fi 
 #something like: (Configure wlan1 to be a WiFi Client)
 #iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
 #iptables -A FORWARD -i wlan0 -o wlan1 -m state --state RELATED,ESTABLISHED -j ACCEPT
