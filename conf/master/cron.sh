9601358123383a2db5ae0cdedab2f088d182f64a
Jean-Frederic Clere <jfclere@gmail.com>
2023-09-30 14:56:59 +0200

    Allow to connect the USB wifi anywhere (like on a usb hub).

--- conf/master/cron.sh
+++ conf/master/cron.sh
@@ -1,7 +1,9 @@
-LAN=wlp0s20f0u1
+# in fact it could be wlp0s20f0u5u1u3 how find that?
+LAN=`/usr/bin/dmesg | /usr/bin/grep rt2800usb | /usr/bin/grep renamed | /usr/bin/tail -1 |  /usr/bin/awk ' { print $4 } ' | /usr/bin/sed 's;:;;'`
 /usr/sbin/iw $LAN info
 if [ $? -ne 0 ]; then
-  LAN=wlan0
+  /usr/bin/echo "failed $LAN not found"
+  exit 1
 fi
 /usr/sbin/ifconfig $LAN | /usr/bin/grep  10.0.0.201
 if [ $? -ne 0 ]; then
