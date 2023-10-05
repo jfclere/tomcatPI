b43b7d1a6b2316359cc807a352cb839627b72308
Jean-Frederic Clere <jfclere@gmail.com>
2023-10-05 19:01:07 +0200

    Add the dmesg part and put the if at the right place!

--- conf/master/cron.sh
+++ conf/master/cron.sh
@@ -1,12 +1,12 @@
 # in fact it could be wlp0s20f0u5u1u3 how find that?
-LAN=`/usr/bin/dmesg | /usr/bin/grep rt2800usb | /usr/bin/grep renamed | /usr/bin/tail -1 |  /usr/bin/awk ' { print $4 } ' | /usr/bin/sed 's;:;;'`
+LAN=`/usr/bin/dmesg -T | /usr/bin/grep rt2800usb | /usr/bin/grep renamed | /usr/bin/tail -1 |  /usr/bin/awk ' { print $8 } ' | /usr/bin/sed 's;:;;'`
+echo "Using wlan: $LAN"
 /usr/sbin/iw $LAN info
 if [ $? -ne 0 ]; then
   /usr/bin/echo "failed $LAN not found"
   exit 1
 fi
-/usr/sbin/ifconfig $LAN | /usr/bin/grep  10.0.0.201
-INTER=`sudo grep ^interface= /etc/hostapd/hostapd.conf |  /usr/bin/awk -F= ' { print $2 } '`
+INTER=`/usr/bin/sudo /usr/bin/grep ^interface= /etc/hostapd/hostapd.conf |  /usr/bin/awk -F= ' { print $2 } '`
 if [ $INTER != $LAN ]; then
   /usr/bin/echo " Please use $INTER as interface in /etc/hostapd/hostapd.conf"
   exit 1
@@ -20,6 +20,7 @@ if [ $? -eq 0 ]; then
   /usr/bin/echo "Try: rfkill unblock $RFID and retry"
   exit 1
 fi
+/usr/sbin/ifconfig $LAN | /usr/bin/grep  10.0.0.201
 if [ $? -ne 0 ]; then
    /usr/bin/sudo /usr/sbin/ifconfig $LAN 10.0.0.201 netmask 255.255.255.0
    /usr/bin/sleep 3
